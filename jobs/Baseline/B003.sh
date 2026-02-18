3631e06a7bf1dcdb299024a414c09be820ba6afa
Zhiyuan <michaellu366@gmail.com>
2026-02-18 16:51:31 +1100

    feat: update SSL method to SwAV in baseline job script

--- jobs/Baseline/B003.sh
+++ jobs/Baseline/B003.sh
@@ -20,7 +20,7 @@ mkdir -p logs/Baseline
 
 python3 -m src.train \
   --task pretrain \
-  --ssl-method simclr \
+  --ssl-method swav \
   --dataset soybean --root ./data \
   --arch resnet18 \
   --img-size 224 \
@@ -28,7 +28,17 @@ python3 -m src.train \
   --batch-size 256 \
   --num-workers 4 \
   --lr 3e-4 --weight-decay 1e-4 \
-  --ssl-proj-dim 128 --ssl-hidden-dim 2048 --ssl-temperature 0.2 \
+  --ssl-hidden-dim 2048 \
+  --swav-feat-dim 128 \
+  --swav-n-prototypes 3000 \
+  --swav-temperature 0.1 \
+  --swav-epsilon 0.05 \
+  --swav-sinkhorn-iters 3 \
+  --swav-crops-for-assign 0 1 \
+  --swav-nmb-crops 2 \
+  --swav-size-crops 224 \
+  --swav-min-scale-crops 0.14 \
+  --swav-max-scale-crops 1.0 \
   --linear-epochs 20 --linear-lr 1e-2 --knn-k 20 --knn-t 0.1 \
   --seed 42 --device cuda \
-  >> logs/Baseline/B000.log 2>&1
+  >> logs/Baseline/B003.log 2>&1
