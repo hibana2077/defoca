deb90db084d0724d354a747230c03b4e9d0e021a
hibana2077 <hibana2077@gmail.com>
2026-02-19 17:52:27 +1000

    feat: add training script for multiple architectures in conv.sh

---
+++ jobs/local/conv.sh
@@ -0,0 +1,22 @@
+cd ../../
+
+python3 -u python -m src.train \
+    --dataset soybean --root ./data \
+    --defoca --P 4 --ratio 0.25 --sigma 1.0 \
+    --strategy contiguous --V 8 --epochs 200 \
+    --arch resnet18 \
+    >> logs/r18.log 2>&1
+
+python3 -u python -m src.train \
+    --dataset soybean --root ./data \
+    --defoca --P 4 --ratio 0.25 --sigma 1.0 \
+    --strategy contiguous --V 8 --epochs 200 \
+    --arch resnet50 \
+    >> logs/r50.log 2>&1
+
+python3 -u python -m src.train \
+    --dataset soybean --root ./data \
+    --defoca --P 4 --ratio 0.25 --sigma 1.0 \
+    --strategy contiguous --V 8 --epochs 200 \
+    --arch gcvit_tiny.in1k \
+    >> logs/gcvit.log 2>&1
