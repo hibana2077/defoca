7cf47a5eca157bf10a29980110eb644985c57baf
hibana2077 <hibana2077@gmail.com>
2026-02-19 12:31:41 +1000

    fix: add -u flag to python3 command for unbuffered output in DS000.sh

--- jobs/defoca_ssl/DS000.sh
+++ jobs/defoca_ssl/DS000.sh
@@ -61,7 +61,7 @@ LOG_FILE="$LOG_DIR/DS000_idx${IDX}_seed${SEED}.log"
   echo "==========================="
 } >> "$LOG_FILE"
 
-python3 -m src.train \
+python3 -u -m src.train \
   --task pretrain \
   --ssl-method simclr \
   --dataset cifar100 --root ./data \
